70ff73f817c6a7093aa3fb1dd336d5a585a6820b
Umesh patel <100286708+umesh6342@users.noreply.github.com>
2022-03-15 23:51:46 +0530

    remove response.js

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -2,7 +2,6 @@ import React from 'react'
 import "./SearchPage.css"
 import { useStateValue } from './StateProvider'
 import useGoogleSearch from './useGoogleSearch';
-import Response from './Response';
 import { Link } from "react-router-dom";
 import Search from './Search';
 import SearchIcon from '@mui/icons-material/Search';
